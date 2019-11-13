d461440206833a9b573137f0c2aebde61c9d38a4
pcollum <petercollum@hee.nhs.uk>
2019-11-13 14:04:43 +0000

    remove ga config code

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,7 @@
 module.exports = {
     plugins: [
 
-      {
-            resolve: `gatsby-plugin-google-analytics`,
-            options: {
-              // replace "UA-XXXXXXXXX-X" with your own Tracking ID
-              trackingId: "UA-152429901-1",
-            },
-          },
+
 
                 {
         resolve: `gatsby-plugin-typography`,
