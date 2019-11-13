30b7563fdd85f4085fb95adf1ac92db44bd5de71
pcollum <petercollum@hee.nhs.uk>
2019-11-13 14:12:39 +0000

    moved ; on line 9

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,7 @@ export default () => (
       <script async src="https://www.googletagmanager.com/gtag/js?id=UA-152429901-1"></script>
       <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+        function gtag(){dataLayer.push(arguments)};
         gtag('js', new Date());
 
         gtag('config', 'UA-152429901-1');
